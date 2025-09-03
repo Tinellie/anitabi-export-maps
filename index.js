6778e3554d9f5fd4e0aab1140df61bae6f721482
Tinellie <akizukimiu23@gmail.com>
2025-09-03 18:51:32 +0800

    feat

--- index.js
+++ index.js
@@ -111,16 +111,6 @@ function getPaletteColor() {
 
 // endregion
 
-let a = {
-    114: {id: 114, name: "かぐや様は告らせたい～天才たちの恋愛頭脳戦～", raw: ["kgy1-0", "kgy1-1"], count: 1},
-    115: {id: 115, name: "かぐや様は告らせたい？～天才たちの恋愛頭脳戦～", raw: ["kgy2-0", "kgy2-1"], count: 1},
-    514: {id: 514, name: "小林さんちのメイドラゴンS", raw: ["dragon1"], count: 1},
-    314: {id: 314, name: "小林さんちのメイドラゴン", raw: ["dragon2", "dragon3"], count: 1},
-    124: {id: 124, name: "かぐや様は告らせたい～天才たちの恋愛頭脳戦!!", raw: ["kgy3-0"], count: 1},
-    1919: {id: 125, name: "ジョジョの奇妙な冒険 黄金の風", raw: ["jojo-gw"], count: 1},
-    810: {id: 124, name: "ジョジョの奇妙な冒険 スターダストクルセイダース", raw: ["jojo-sc"], count: 1},
-    125: {id: 125, name: "かぐや様は告らせたい？～天才たちの恋愛頭脳戦!!??", raw: ["kgy4-0", "kgy4-1"], count: 1},
-};
 
 // main().then()
 
@@ -160,7 +150,7 @@ function dict_to_array(dict) {
 }
 function array_to_dict(arr, get_key) {
     let dict = {}
-    for (let a of arr) { dict[get_key(a)] = a }
+    for (let idx in arr) { dict[get_key(arr[idx])] = arr[idx] }
     return dict
 }
 
@@ -178,12 +168,29 @@ function processingAnimeEntry(anime) {
 }
 
 function merge(a1, a2) {
+    console.log(a1, a2);
+    if (!a1?.points || !a2?.points) return;
     a1.name = a1.name + " / " + a2.name;
     a1.count += a2.count;
     a1.points.push(...a2.points);
 }
 function remove(arr, index) {
     return arr.filter((v, i) => i !== index);
+}
+
+
+function mergeAll(animes, ids) {
+    animes = dict_to_array(animes);
+    if (ids.length < 1) return;
+
+    let a1 = animes.find(a => a.id === ids[0])
+    for (let i = 1; i < ids.length; i++) {
+        let j = animes.findIndex(a => a.id === ids[i])
+        let a2 = animes[j];
+        merge(a1, a2);
+        animes = remove(animes, j);
+    }
+    return array_to_dict(animes, x => x.id);
 }
 function autoMergingByAnime(animes) {
     animes = dict_to_array(animes);
@@ -284,7 +291,7 @@ async function getAnimeCollectionsPoints(animes_info) {
     let animes = {}
     for (let {id, name} of animes_info) {
         let points = await getAnimePoints(id)
-        if (!points || points.length < 1) continue;
+        if (!points || points.length < 1) points = [];
         animes[id] = { id, name, points, count: 1 };
     }
     return animes;
@@ -316,7 +323,7 @@ async function getBangumiCollections(id, subject = 2, type = 2) {
 
 // region gui
 
-const PANEL_HEIGHT = 400;
+const PANEL_HEIGHT = 500;
 const PANEL_HEADER_HEIGHT = 50;
 const CSS = `
 #export-map-panel {
@@ -324,6 +331,7 @@ const CSS = `
     --c-darker: #aaaa;
     --c-primary: #38a6f4;
     --c-primary-dark: #2266bd;
+    --c-error: #f15151;
 
     position: fixed;
     right: 50px; bottom: 30px;
@@ -337,11 +345,57 @@ const CSS = `
     
     overflow: hidden;
     transition: 0.12s ease-out;
-}
-
-#export-map-panel * {
-    box-sizing: border-box;
-    border: none;
+    
+    & span {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        gap: 5px;
+        height: fit-content;
+    }
+    
+    & list {
+        list-style: none;
+    }
+    & button {
+        flex: 1;
+        height: 30px;
+        
+        border: none;
+        outline: none;
+        padding: 0 10px;
+        
+        border-radius: 8px;
+        background-color: var(--c-primary);
+        color: white;
+        
+        cursor: pointer;
+        transition: 0.04s ease-out;
+        &:hover {
+            background-color: var(--c-primary-dark);
+        }
+        &:active {
+            transform: scale(1.05);
+        }
+    }
+    
+    & input[type=text] {
+        background-color: var(--c-dark);
+        border: none;
+        outline: none;
+        border-radius: 8px;
+        padding: 5px;
+        
+        text-align: center;
+        
+        &:focus {
+            background-color: var(--c-darker);
+        }
+    }
+    & * {
+        box-sizing: border-box;
+        border: none;
+    }
 }
 
 #export-map-panel-header {
@@ -363,11 +417,69 @@ const CSS = `
     width: 100%;
     height: 100%;
     padding: 10px 22px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    overflow: hidden;
 }
 
+#anime-list-wrapper {
+    border-radius: 3px;
+    border: 1px solid var(--c-darker);
+    padding: 0px;    
+    
+    height: 200px;
+}
 #anime-list {
-    height: 100px;
+    height: 200px;
     overflow-y: scroll;
+    
+    
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    
+    & .anime-list-item {
+        padding: 3px;
+        
+        display: flex;
+        flex-direction: row;
+        align-items: baseline;
+        gap: 3px;
+        
+        border-radius: 2px;
+        
+        transition: background-color 0.07s ease-out;
+        &:hover {
+            background-color: var(--c-dark);
+        }
+        &:active {
+            background-color: var(--c-darker);
+        }
+        
+        & input[type=checkbox] {
+            margin: 0;
+            accent-color: var(--c-primary);
+        }
+        
+        & .anime-list-item-count {
+            height: 100%;
+            width: 30px;
+            font-size: 12px;
+            line-height: 15px;
+            text-align: center;
+            color: white;
+            border-radius: 99px;
+        }
+        & p {
+            font-size: 14px;
+            line-height: 15px;
+            
+            flex: 1;
+            width: 0;
+        }
+    }
+    
 }
 
 #bgm-panel {
@@ -391,44 +503,17 @@ const CSS = `
     & > #bgm-id-input {
         flex: 1;
         width: 0;
-        
-        background-color: var(--c-dark);
-        border: none;
-        outline: none;
-        border-radius: 10px;
-        padding: 5px;
-        
-        text-align: center;
     }
     
     & > #bgm-fetch-button {
-        box-sizing: border-box;
         height: 100%;
-        
-        border: none;
-        outline: none;
-        padding: 0 10px;
-        
-        border-radius: 10px;
-        background-color: var(--c-primary);
-        color: white;
-        
-        cursor: pointer;
-        transition: 0.04s ease-out;
-        &:hover {
-            background-color: var(--c-primary-dark);
-        }
-        &:active {
-            transform: scale(1.05);
-        }
+        flex: none;
     }
 }
-#anime-list {
-    list-style: none;
-    padding: 0;
-    
-    & input[type=checkbox] {
-        accent-color: var(--c-primary);
+
+#list-edit-wrapper {
+    & #delete-button {
+        background-color: var(--c-error);
     }
 }
 `
@@ -440,20 +525,103 @@ function addElement(tag, attr = {}) {
     return element;
 }
 
-const user = {
-    collection: [],
-    selected_animes: [],
-    select(id) {
-        this.selected_animes.push(id);
-    },
-    unselect(id) {
-        this.selected_animes.filter(selected => selected !== id);
-    },
-    is_selected(id) {
-        return this.selected_animes.includes(id);
-    },
-    bgm_id: undefined,
-}
+const user = (() => {
+    return {
+        init () {
+            console.warn("init");
+            console.warn(this);
+            this.on_update_data.push(() => {
+                this.clean_selected.call(this);
+                localStorage.setItem("collections", JSON.stringify(this.collections));
+            });
+            return this;
+        },
+
+        call (func_array, params = []) {
+            console.log(func_array)
+            for (let func of func_array) {
+                func(...params)
+            }
+        },
+        collections: JSON.parse(localStorage.getItem("collections")) ?? [],
+        set_collections(collections) {
+            this.collections = dict_to_array(collections);
+            let req = []
+            for (let collection of collections) {
+                if (!Array.isArray(collection.points)) {
+                    req.push(collection)
+                }
+            }
+            if (req.length > 0) {
+                getAnimeCollectionsPoints(req).then((res) => {
+                    user.save_data(res)
+                })
+
+            }
+            else {
+                this.call(this.on_update_data);
+            }
+        },
+        clear_collections () {
+            this.set_collections([]);
+        },
+        clean_selected() {
+            this.selected_animes = this.selected_animes.filter(x => this.collections.find(anime => anime.id === x.id));
+        },
+        on_update_data: [],
+        save_data(res) {
+            for (let anime of this.collections){
+                let data = res[anime.id];
+                console.log(data);
+                for (let key in data) {
+                    anime[key] = data[key];
+                }
+            }
+            this.call(this.on_update_data);
+        },
+
+        selected_animes: [],
+        all_selected: 0,
+
+        on_selected: [],
+
+        update_selected_state() {
+            if (this.selected_animes.length >= this.collections.length && this.collections.every(x => this.selected_animes.includes(x.id))) {
+                this.all_selected = 2;
+            }
+            else if (this.selected_animes.length > 0) {
+                this.all_selected = 1;
+            }
+            else {
+                this.all_selected = 0;
+            }
+
+            this.call(this.on_selected, [this.selected_animes.length, this.get_selected_collection()])
+        },
+
+        select(id) {
+            this.select_all([id])
+        },
+        select_all(ids = this.collections.map(item => item.id)) {
+            this.selected_animes.push(...ids);
+            this.update_selected_state()
+        },
+        unselect(id) {
+            this.unselect_all([id])
+        },
+        unselect_all(ids = this.selected_animes) {
+            this.selected_animes = this.selected_animes.filter(selected => !ids.includes(selected));
+            this.update_selected_state()
+        },
+        is_selected(id) {
+            return this.selected_animes.includes(id);
+        },
+        get_selected_collection() {
+            return this.collections.filter(x => this.selected_animes.includes(x.id));
+        },
+        bgm_id: localStorage.getItem("bgm_id"),
+    }.init()
+})()
 
 function addPanel(parent) {
     let panel_collapsed = false;
@@ -480,28 +648,30 @@ function addPanel(parent) {
 
     let panel_content = addElement("div", {id: "export-map-panel-content"});
 
+
     let anime_list_wrapper = addElement("div", {id: "anime-list-wrapper"});
     let anime_list = getAnimeListComponent()
+    anime_list_wrapper.appendChild(anime_list);
+    let refresh_list = () => {
+        let new_list = getAnimeListComponent();
+        anime_list_wrapper.replaceChild(new_list, anime_list);
+        anime_list = new_list;
+    }
+    user.on_selected.push(refresh_list);
+    user.on_update_data.push(refresh_list);
 
-    let bgm_panel = addElement("span", {id: "bgm-panel"});
-    let bgm_id_input_label = addElement("label", {id: "bgm-id-input-label", innerText: "Bangumi ID"});
-    let bgm_id_input = addElement("input", {id: "bgm-id-input", type: "text", onchange: e => {
-        user.bgm_id = e.target.value;
-    }});
-    let bgm_fetch_button = addElement("button", {
-        id: "bgm-fetch-button",
-        innerText: "获取",
-        onclick: ev => {
-            getBangumiCollections(user.bgm_id).then((res) => {
-                user.collection = res;
-                let new_list = getAnimeListComponent();
-                panel_content.replaceChild(new_list, anime_list);
-                anime_list = new_list;
-            })
-        }});
-    bgm_panel.append(bgm_id_input_label, bgm_id_input, bgm_fetch_button)
+    let bgm_panel = getBangumiComponent(refresh_list);
+    let select_all_component = getSelectAllComponent(refresh_list);
+    let list_edit_component = getListEditComponent(refresh_list);
+    let get_points_component = getGetPointsComponent(refresh_list);
 
-    panel_content.append(bgm_panel, anime_list)
+    panel_content.append(
+        bgm_panel,
+        anime_list_wrapper,
+        select_all_component,
+        list_edit_component,
+        get_points_component,
+    );
 
     panel.append(panel_header, panel_content);
 
@@ -511,23 +681,149 @@ function addPanel(parent) {
     return panel;
 }
 
+function getBangumiComponent(refresh_list) {
+
+    let bgm_panel = addElement("span", {id: "bgm-panel"});
+    let bgm_id_input_label = addElement("label", {id: "bgm-id-input-label", innerText: "Bangumi ID"});
+    let bgm_id_input = addElement("input", {id: "bgm-id-input", type: "text", value: user.bgm_id, onchange: e => {
+            user.bgm_id = e.target.value;
+            localStorage.setItem("bgm_id", user.bgm_id);
+        }});
+    let bgm_fetch_button = addElement("button", {
+        id: "bgm-fetch-button",
+        innerText: "获取",
+        onclick: ev => {
+            getBangumiCollections(user.bgm_id).then((res) => {
+                user.set_collections(res);
+            })
+        }});
+    bgm_panel.append(bgm_id_input_label, bgm_id_input, bgm_fetch_button)
+
+    return bgm_panel
+}
+
+
+function getSelectAllComponent() {
+    let select_all_wrapper = addElement("span", {id: "list-edit-wrapper"});
+    let list_select_all_button = addElement("button", {
+        id: "list-select-all-button",
+    })
+    list_select_all_button.onclick = ev => {
+        if (user.all_selected > 0)
+            user.unselect_all();
+        else
+            user.select_all();
+    }
+    let button_text = "全选";
+    let filtered_selected = user.selected_animes.filter(id => user.collections.find(anime => anime.id === id));
+    console.log()
+    let entries_count = filtered_selected.length;
+    let points_count = [0, ...filtered_selected]
+        .reduce((prev, current) => (current?.points?.length ?? 0) + prev);
+    let refresh_button_text = () => {
+        list_select_all_button.innerText = button_text + ` (${entries_count}条目, ${points_count}地点)`
+    }
+    refresh_button_text();
+    user.on_update_data.push(refresh_button_text);
+
+    user.on_selected.push((count, selected) => {
+        if (user.all_selected > 0) {
+            button_text = "取消选择";
+        }
+        else {
+            button_text = "全选";
+        }
+        refresh_button_text();
+    });
+
+    select_all_wrapper.append(list_select_all_button);
+    return select_all_wrapper;
+}
+
+
+function getListEditComponent() {
+    let list_edit_wrapper = addElement("span", {id: "list-edit-wrapper"})
+
+    let delete_button = addElement("button", {
+        id: "delete-button",
+        innerText: "删除",
+        onclick: ev => {
+            user.set_collections(
+                user.collections.filter(x => !user.selected_animes.includes(x.id))
+            );
+        }
+    })
+    let merge_button = addElement("button", {
+        id: "merge-button",
+        innerText: "合并",
+        onclick: ev => {
+            user.set_collections(
+                dict_to_array(mergeAll(user.collections, user.selected_animes))
+            );
+        }
+    })
+    let auto_merge_button = addElement("button", {
+        id: "auto-merge-button",
+        innerText: "自动合并",
+        onclick: ev => {
+            user.set_collections(
+                dict_to_array(autoMergingByAnime(user.collections.filter(x => x.points && x.points.length > 0)))
+            );
+        }
+    })
+
+    list_edit_wrapper.append(delete_button, merge_button, auto_merge_button);
+    return list_edit_wrapper;
+}
+
+
+function getGetPointsComponent() {
+    let get_points_button_wrapper = addElement("span", {id: "get-points-button-wrapper"});
+    let get_points_button = addElement("button", {
+        id: "get-points-button",
+        className: "expm-button",
+        innerText: "获取巡礼点",
+        onclick: ev => {
+            ev.target.style.backgroundColor = "var(--c-darker)"
+            getAnimeCollectionsPoints(user.get_selected_collection()).then((res) => {
+                ev.target.style.backgroundColor = null
+                console.log(res);
+                user.save_data(res)
+            })
+        }});
+    get_points_button_wrapper.appendChild(get_points_button);
+    return get_points_button_wrapper;
+}
+
 function getAnimeListComponent() {
     let anime_list = addElement("ul", {id: "anime-list"});
     console.warn(user)
-    for (let anime of user.collection) {
-        let li = addElement("li", {className: "anime-list-item"});
-        li.append(addElement("input", {
+    for (let anime of user.collections) {
+        let checkbox = addElement("input", {
             className: "anime-list-checkbox",
             type: "checkbox",
-            checked: user.selected_animes.includes(anime.id),
+            checked: user.selected_animes.includes(anime.id)
+        })
+        let li = addElement("li", {
+            className: "anime-list-item",
             onclick: () => {
-                if (user.is_selected(anime.id))
+                if (user.is_selected(anime.id)){
                     user.unselect(anime.id);
-                else
+                    checkbox.checked = false;
+                }
+                else {
                     user.select(anime.id);
+                    checkbox.checked = true;
+                }
             }
-        }));
-        li.append(document.createTextNode(anime.name));
+        });
+        let count = user?.collections?.find(x => x.id === anime.id)?.points?.length ?? 0;
+        let counter = addElement("div", {className: "anime-list-item-count", innerText: `${count}`})
+        counter.style.backgroundColor = count === 0 ? `#888` : `hsl(${(1 - Math.log10(count) / 3) * 200}deg 80% 50%)`;
+        li.append(
+            checkbox,
+            counter,
+            addElement("p", {innerText: anime.name}));
         anime_list.append(li);
     }
     return anime_list
